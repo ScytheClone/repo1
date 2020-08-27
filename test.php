483be18fceaa8a43a5f82cbe4456f3917d043ec9
Gustav Christiansson <gustav.christiansso@learnet.se>
2020-08-27 09:13:00 +0200

    lagt till comitten

---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>Hello World</div>
+    
+        <?php
+            $bacon=0;
+        while($bacon<10){
+            echo "<div>Det funkar </div>";
+            $bacon++;
+        }
+        $sauce=0;
+        while($sauce<10){
+            echo "<div>Gör det verkligen det dock </div>";
+            $sauce++;
+        }
+     ?>
+         
+</body>
+</html>
